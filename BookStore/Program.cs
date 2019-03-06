337bbf9a8ebbe95d431ca19cf687760c6c599c11
soakes84 <spencer.oakes84@gmail.com>
2019-03-06 15:52:19 -0500

    hard coded in a book

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -6,7 +6,15 @@ namespace BookStore
     {
         static void Main(string[] args)
         {
-            
+            Book actionBook = new Book("The House of Spies", "Daniel Silva", new DateTime(2016, 12, 11), 19.95m);
+
+            decimal price = actionBook.CalculateBookStorePrice();
+
+            Console.WriteLine($"Title: {actionBook.Title}");
+            Console.WriteLine($"Author: {actionBook.Author}");
+            Console.WriteLine($"Published On: {actionBook.PublishDate}");
+            Console.WriteLine($"Price: {price:C}");
+            Console.ReadLine();
         }
     }
 }
