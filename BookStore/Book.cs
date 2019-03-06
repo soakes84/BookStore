f2191140578751b30866d22bd668b8f88377fc01
soakes84 <spencer.oakes84@gmail.com>
2019-03-06 15:36:40 -0500

    Book class

---
+++ BookStore/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore
+{
+    public class Book
+    {
+        private string title;
+        private string author;
+
+        private DateTime publishDate;
+
+        private decimal price;
+
+        public Book(string title, string author, DateTime publishDate, decimal price)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.PublishDate = publishDate;
+            this.Price = price;
+        }
+
+        public string Title { get => title; set => title = value; }
+        public string Author { get => author; set => author = value; }
+
+        public DateTime PublishDate { get => publishDate; set => publishDate = value; }
+
+        public decimal Price { get => price; set => price = value; }
+
+    }
+}
--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -6,7 +6,7 @@ namespace BookStore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
